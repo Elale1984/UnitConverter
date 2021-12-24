d7064524ea02051b839c5be508c4ad1f445d869e
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-12-23 22:42:16 -0600

    Added the logic for the conversions and fixed some errors with the UI

--- app/src/main/java/com/headyapps/unitconverter/MainViewModel.kt
+++ app/src/main/java/com/headyapps/unitconverter/MainViewModel.kt
@@ -18,14 +18,58 @@ class MainViewModel : ViewModel() {
         convertInput: String,
     ) = viewModelScope.launch {
 
-        if(convertFrom != "" && convertTo != "" && convertInput != "") {
+        if(convertFrom != "" && convertTo != "" && convertInput != "" && convertFrom != convertTo) {
 
             _conversionState.value = ConversionUIState.Loading
             delay(500L)
 
 
-            val conversionInput = convertInput.toInt()
-            val convertedResult = conversionInput * 5
+            val conversionInput = convertInput.toDouble()
+            var convertedResult = 0.0
+
+
+            when(convertFrom){
+                "Cups" -> {
+                    when(convertTo){
+                        "Ounces" -> convertedResult = conversionInput * 8.0
+                        "Gallon" -> convertedResult = conversionInput / 16.0
+                        "Pints"  -> convertedResult = conversionInput / 2.0
+                        "Liters" -> convertedResult = conversionInput / 4.227
+                    }
+                }
+                "Ounces" -> {
+                    when(convertTo){
+                        "Cups" -> convertedResult = conversionInput / 8.0
+                        "Gallon" -> convertedResult = conversionInput / 128.0
+                        "Pints"  -> convertedResult = conversionInput / 16.0
+                        "Liters" -> convertedResult = conversionInput / 33.814
+                    }
+                }
+                "Gallons" -> {
+                    when(convertTo){
+                        "Cups" -> convertedResult = conversionInput * 16
+                        "Ounces" -> convertedResult = conversionInput * 128.0
+                        "Pints"  -> convertedResult = conversionInput * 8
+                        "Liters" -> convertedResult = conversionInput * 3.785
+                    }
+                }
+                "Pints" -> {
+                    when(convertTo){
+                        "Cups" -> convertedResult = conversionInput * 2.0
+                        "Ounces" -> convertedResult = conversionInput * 16.0
+                        "Gallons"  -> convertedResult = conversionInput / 8.0
+                        "Liters" -> convertedResult = conversionInput / 2.113
+                    }
+                }
+                "Liters" -> {
+                    when(convertTo){
+                        "Cups" -> convertedResult = conversionInput * 4.227
+                        "Ounces" -> convertedResult = conversionInput * 33.814
+                        "Gallons"  -> convertedResult = conversionInput / 3.785
+                        "Pints" -> convertedResult = conversionInput * 2.113
+                    }
+                }
+            }
 
             _conversionState.value = ConversionUIState.Success(convertedResult, convertTo)
 
@@ -37,7 +81,7 @@ class MainViewModel : ViewModel() {
         }
     }
     sealed class ConversionUIState {
-        data class Success(val result: Int, val convertedType: String) : ConversionUIState()
+        data class Success(val result: Double, val convertedType: String) : ConversionUIState()
         data class Error(val message: String) : ConversionUIState()
         object Loading : ConversionUIState()
         object Empty : ConversionUIState()
